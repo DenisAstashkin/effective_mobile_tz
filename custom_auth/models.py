afb391cd9f278d7870c701105a423eca7f755d0b
DenisAstashkin <ast.den27@gmail.com>
2026-07-06 00:31:54 +0400

    Add model

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -1,3 +1,48 @@
+import uuid
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class CustomUser(models.Model):
+    full_name = models.CharField(max_length=255, null=False)
+    email = models.EmailField(max_length=255, unique=True, null=False)
+    password_hash = models.CharField(max_length=255, null=False)
+    is_active = models.BooleanField(default=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'users'
+        
+class UserSession(models.Model):
+    token = models.CharField(max_length=64, primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sessions')
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(null=False)
+
+    class Meta:
+        db_table = 'user_sessions'
+
+    def is_expired(self):
+        return timezone.now() > self.expires_at
+
+class Resource(models.Model):
+    name = models.CharField(max_length=100, unique=True, null=False)
+
+    class Meta:
+        db_table = 'resources'
+
+class AccessRule(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE, null=False)
+    
+    read_all_permission = models.BooleanField(default=False, null=False)
+    read_own_permission = models.BooleanField(default=False, null=False)
+    create_permission = models.BooleanField(default=False, null=False)
+    update_all_permission = models.BooleanField(default=False, null=False)
+    update_own_permission = models.BooleanField(default=False, null=False)
+    delete_all_permission = models.BooleanField(default=False, null=False)
+    delete_own_permission = models.BooleanField(default=False, null=False)
+    is_admin_permission = models.BooleanField(default=False, null=False)
+
+    class Meta:
+        db_table = 'access_rules'
+        unique_together = ('user', 'resource')
